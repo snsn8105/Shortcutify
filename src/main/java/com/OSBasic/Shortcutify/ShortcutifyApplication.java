af1b8fc2e124a82c8450910926404688c379430c
snsn8105 <snsn8105@naver.com>
2025-05-02 17:37:57 +0900

    chore: Spring initialization

---
+++ src/main/java/com/OSBasic/Shortcutify/ShortcutifyApplication.java
@@ -0,0 +1,13 @@
+package com.OSBasic.Shortcutify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortcutifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortcutifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/OSBasic/Shortcutify/ShortcutifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OSBasic.Shortcutify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortcutifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
