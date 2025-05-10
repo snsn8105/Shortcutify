252b53a1d1f21628b9897bbb4e604fcc5a5830e9
*이수빈* <subinlee05@gmail.com>
2025-05-11 00:48:46 +0900

    ahhhh

---
+++ front_react/front/src/index.js
@@ -0,0 +1,20 @@
+// src/index.js
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
+
+// 웹 바이탈 측정 함수를 호출합니다.
+reportWebVitals();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
